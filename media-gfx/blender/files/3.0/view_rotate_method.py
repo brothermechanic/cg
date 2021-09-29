458801dfd3dc35b2591e297998e46a54b5dbc2f5
brothermechanic <brothermechanic@gmail.com>
2021-09-29 10:07:31 +0300

    add cg defaults

--- media-gfx/blender/files/3.0/view_rotate_method.py
+++ media-gfx/blender/files/3.0/view_rotate_method.py
@@ -3,6 +3,7 @@ from bpy.app.handlers import persistent
 @persistent
 def rotate_method(dummy):
     bpy.context.preferences.inputs.view_rotate_method = "TRACKBALL"
+    bpy.context.preferences.addons['mesh_f2'].preferences.autograb=False
 def register():
     bpy.app.handlers.load_post.append(rotate_method)
 def unregister():
